e1bf5ee1e5b3a981bd2de56de69dc01f296f78fb
jliudong <jliudong@gmail.com>
2017-03-16 19:41:21 +0800

    提交代码

---
+++ LaunchMode/app/src/main/java/com/diandiyun/launchmode/MainActivity.java
@@ -0,0 +1,35 @@
+package com.diandiyun.launchmode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.tv);
+        tv.setText(String.format("TaskID:%d\n%s", getTaskId(), toString()));
+
+        findViewById(R.id.btnStartMainAty).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
+
+        findViewById(R.id.btnStartBAty).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BAty.class));
+            }
+        });
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Android-LaunchMode
+# Android-LaunchMode #
 Android 中 Activity 启动模式
+
+## 课程背景： ##
+在 Android 中 Activity 有四种启动模式，每一种启动模式都代表着一种使用场合，对四种启动模式有深入的了解才能够在实际开发工作中得心应手，写出完美的程序。
+
+## 核心内容： ##
+1.Activity Standard 启动模式
+2.Activity SingleTop 启动模式
+3.Activity SingleTask 启动模式
+4.Activity SingleInstance 启动模式
+
+软件环境：Android Studio
